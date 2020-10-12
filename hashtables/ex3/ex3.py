f21dc206404ebe635e2f3f5991380119e494c655
fiopwk <dajuan-mcdonald@lambdastudents.com>
2020-10-12 08:36:15 -0400

    intersection fixed

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,20 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+
+    length_of_lists = len(arrays)
+
+    for list_of_integers in arrays:
+        for integer in list_of_integers:
+            if integer in cache:
+                cache[integer] += 1
+            else:
+                cache[integer] = 1
+
+            if cache[integer] == length_of_lists:
+                result.append(integer)
 
     return result
 
