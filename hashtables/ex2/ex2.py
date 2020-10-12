181296fd8f387963f3992f35843dd515d37565c2
fiopwk <dajuan-mcdonald@lambdastudents.com>
2020-10-12 08:39:00 -0400

    reconstruct_trip fixed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,19 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    #store all flights into a dictionary key:source -> val:destination
+    flights = {}
+    route = [None] * length
+
+    for i in range(0, length):
+        t = tickets[i]
+       flights[t.source] += [t.destination]
+
+    route[0] = flights["NONE"]
+    route[1] = flights[route[0]]
+
+    if length > 2:
+        for i in range(2, length):
+            route[i] = flights[route[i-1]]
 
     return route
